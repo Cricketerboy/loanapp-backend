9a13e64ad90142041d743a1554934fccfd43a961
Mohit Anand <praveenanand801@gmail.com>
2025-02-23 19:22:48 +0530

    update

--- models/User.js
+++ models/User.js
@@ -13,12 +13,6 @@ const userSchema = new mongoose.Schema({
   panNumber: { type: String, unique: true, sparse: true },
 });
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+
 
 module.exports = mongoose.model("User", userSchema);
